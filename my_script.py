f6e2bb78b128ce3ba04fe050c32228b858037f54
root <root@localhost.localdomain>
2023-11-02 12:57:30 +0530

    Updated my_script.py

--- my_script.py
+++ my_script.py
@@ -1,1 +1,12 @@
-print("Hello World By Rik Biswas!")
+import sys
+ 
+# total arguments
+n = len(sys.argv)
+print("Total arguments passed:", n)
+ 
+# Arguments passed
+print("\nName of Python script:", sys.argv[0])
+ 
+print("\nArguments passed:", end = " ")
+for i in range(1, n):
+    print(sys.argv[i], end = " ")
